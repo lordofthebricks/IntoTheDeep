756198bd5a8113c1dd5d2fac3842b002ad6b2b2e
victor <therealpenguinolo@gmail.com>
2024-09-14 16:39:41 -0400

    Added meepmeep to allow for trajectory testing

---
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -0,0 +1,70 @@
+package com.example.meepmeeptesting;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeBlueDark;
+import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeRedDark;
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class MeepMeepTesting {
+    public static void main(String[] args) {
+        MeepMeep meepMeep = new MeepMeep(800);
+
+        // Declare our first bot
+        RoadRunnerBotEntity myFirstBot = new DefaultBotBuilder(meepMeep)
+                // We set this bot to be blue
+                .setColorScheme(new ColorSchemeBlueDark())
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .followTrajectorySequence(drive ->
+                        drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
+                                .forward(30)
+                                .turn(Math.toRadians(90))
+                                .forward(30)
+                                .turn(Math.toRadians(90))
+                                .forward(30)
+                                .turn(Math.toRadians(90))
+                                .forward(30)
+                                .turn(Math.toRadians(90))
+                                .build()
+                );
+
+//        // Declare out second bot
+//        RoadRunnerBotEntity mySecondBot = new DefaultBotBuilder(meepMeep)
+//                // We set this bot to be red
+//                .setColorScheme(new ColorSchemeRedDark())
+//                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+//                .followTrajectorySequence(drive ->
+//                        drive.trajectorySequenceBuilder(new Pose2d(30, 30, Math.toRadians(180)))
+//                                .forward(30)
+//                                .turn(Math.toRadians(90))
+//                                .forward(30)
+//                                .turn(Math.toRadians(90))
+//                                .forward(30)
+//                                .turn(Math.toRadians(90))
+//                                .forward(30)
+//                                .turn(Math.toRadians(90))
+//                                .build()
+//                );
+
+        Image img = null;
+        try { img = ImageIO.read(new File("/home/victor/Documents/programming/robotics/FtcRobotController-IntoTheDeep/MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepIntoTheDeep.png")); }
+        catch (IOException e) {}
+
+
+        meepMeep.setBackground(img)
+                .setDarkMode(true)
+                .setBackgroundAlpha(0.95f)
+
+                // Add both of our declared bot entities
+                .addEntity(myFirstBot)
+//                .addEntity(mySecondBot)
+                .start();
+    }
+}
