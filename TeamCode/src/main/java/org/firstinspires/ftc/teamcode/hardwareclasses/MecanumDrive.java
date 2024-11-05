c28889864b581f7ffadc32c4f2d7b9a1aaef3850
Pengiunolo <46852384+Pengiunolo@users.noreply.github.com>
2024-11-05 16:20:10 -0500

    ???

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardwareclasses/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardwareclasses/MecanumDrive.java
@@ -220,7 +220,7 @@ public final class MecanumDrive {
 
         // TODO: make sure your config has motors with these names (or change them)
         //   see https://ftc-docs.firstinspires.org/en/latest/hardware_and_software_configuration/configuring/index.html
-        leftFront = hardwareMap.get(DcMotorEx.class, "LeftFront");
+        leftFront = hardwareMap.get(DcMotorEx.class, "FrontL");
         leftBack = hardwareMap.get(DcMotorEx.class, "leftBack");
         rightBack = hardwareMap.get(DcMotorEx.class, "rightBack");
         rightFront = hardwareMap.get(DcMotorEx.class, "rightFront");
