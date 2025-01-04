91caeffe79a992fa93bc85bd7e050cfbc0969f03
victor <therealpenguinolo@gmail.com>
2025-01-04 17:16:20 -0500

    began workon odometry testing opmode.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/bombadil2/Bom2OdometryPractice.java
@@ -0,0 +1,33 @@
+package org.firstinspires.ftc.teamcode.bombadil2;
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.MecanumDrive;
+
+public class Bom2OdometryPractice extends LinearOpMode {
+
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        Pose2d beginPose = new Pose2d(0, 0, 0);
+        MecanumDrive drive = new MecanumDrive(hardwareMap, beginPose);
+
+        waitForStart();
+
+        Actions.runBlocking(
+                drive.actionBuilder(beginPose)
+                        .strafeToConstantHeading(new Vector2d(10,0))
+                        .strafeToConstantHeading(new Vector2d(10,10))
+                        .strafeToConstantHeading(new Vector2d(0,10))
+                        .strafeToConstantHeading(new Vector2d(0,0))
+                        .build()
+        );
+
+
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/bombadil2/Bom2odometrypractice.java
+++
@@ -1,4 +0,0 @@
-package org.firstinspires.ftc.teamcode.bombadil2;
-
-public class Bom2odometrypractice {
-}
