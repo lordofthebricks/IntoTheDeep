ab72b567150c2af30a13b285951a7a5e0ee751b5
aidan7039 <coolgamer17856@gmail.com>
2025-01-04 16:46:44 -0500

    new encoder functions that actually work

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/bombadil2/BombadilHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/bombadil2/BombadilHardware.java
@@ -33,9 +33,9 @@ public class BombadilHardware {
     private static final double SLIDE_PULLY_DIAMETER = 1.504;
 
     private static final double SLIDE_TICKS_PER_REV = 384.5;
-
-    private static final double TILT_TICKS_PER_REV = 1425.1;
+    public static final double TILT_TICKS_PER_REV = 1425.1;
     public static final double SLIDE_TICKS_PER_INCH = SLIDE_TICKS_PER_REV/(Math.PI * SLIDE_PULLY_DIAMETER);
+    public static final double TILT_TICKS_PER_INCH = TILT_TICKS_PER_REV * 5.0/(Math.PI * 1.0);
     public static final int MAX_SLIDE_POSITION = (int) (35 * SLIDE_TICKS_PER_INCH);
 
     public int init(HardwareMap hwMap){
